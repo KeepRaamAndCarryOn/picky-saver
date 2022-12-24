3538504440180640d58115a13c18b7fbf534fa0c
Raamkumar Krishnan <raamkumar.krishnan@configura.com>
2022-12-24 20:26:47 +0800

    Create context menu

--- scripts/background.js
+++ scripts/background.js
@@ -8,12 +8,25 @@ chrome.tabs.onCreated.addListener((tab) => {
 
 chrome.downloads.onCreated.addListener((downloadItem) => {
   console.log("background: downloads onCreated ", downloadItem)
-  });
+});
 
 chrome.downloads.onDeterminingFilename.addListener((downloadItem, suggest) => {
-  console.log("background downloads onDeterminingFilename ", downloadItem.filename, downloadItem)
+  console.log("background downloads onDeterminingFilename ", downloadItem.url, downloadItem)
   sugg = {
     filename: "sub/top.pdf" //note automatically creates "sub" folder
   };
   suggest(sugg);
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    "id": "customDownload",
+    "title": "My custom download",
+    "contexts": ["link"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    console.log(info)
+    console.log(tab)
 });
